f2e50ca9855cff6bb0eb975d929ccd675c4645a2
Dongyu Wang <dongyu.wang@monstar-lab.com>
2023-09-26 00:15:10 +0900

    add answer

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <div>
+    <h1>Counter: {{ count }}</h1>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 5rem;
+}
+p {
+  font-size: 2rem;
+}
+button {
+  font-size: 2rem;
+  padding: 1rem 2rem;
+  border: 1px solid blue;
+  border-radius: 0.5rem;
+}
+</style>
--- src/views/hands-on/Component.vue
+++ src/views/hands-on/Component.vue
@@ -1,5 +1,8 @@
-<script setup></script>
+<script setup>
+import Counter from "../../components/Counter.vue";
+</script>
 
 <template>
+  <Counter />
   <p>ここに新しcomponentを挿してください</p>
 </template>
