3ed0e7f0105966aa25fa9f05a6063ef135ce65ae
Dongyu Wang <dongyu.wang@monstar-lab.com>
2023-09-26 00:07:35 +0900

    setup counter hands on

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Responsive from "../views/hands-on/Responsive.vue";
 import BasicComponent from "../views/demo/BasicComponent.vue";
+import Counter from "../views/hands-on/Counter.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,14 +13,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/hands-on/responsive",
-      name: "responsive",
-      component: Responsive,
-    },
-    {
       path: "/demo/component",
       name: "component",
       component: BasicComponent,
+    },
+    {
+      path: "/hands-on/counter",
+      name: "counter",
+      component: Counter,
+    },
+    {
+      path: "/hands-on/responsive",
+      name: "responsive",
+      component: Responsive,
     },
   ],
 });
