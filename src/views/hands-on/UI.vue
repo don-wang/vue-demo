114bd7dd3353eab8f3bada52dc5330957d11d6f2
Dongyu Wang <dongyu.wang@monstar-lab.com>
2023-09-26 09:19:02 +0900

    api user

--- src/views/hands-on/UI.vue
+++ src/views/hands-on/UI.vue
@@ -1,32 +1,35 @@
 <template>
   <p>ここにUI Libraryを利用し、ユーザー情報をキレイに表示してください</p>
-  <h1 class="text-blue-500">Name: {{ user.name }}</h1>
+  <a-card v-if="user">
+    <a-typography-title class="text-blue-500">{{
+      user.name
+    }}</a-typography-title>
+    <a-descriptions title="User Info" bordered>
+      <a-descriptions-item label="Name">{{ user.name }}</a-descriptions-item>
+      <a-descriptions-item label="Email">
+        <a-tag color="green">{{ user.email }}</a-tag></a-descriptions-item
+      >
+      <a-descriptions-item label="Tel">{{ user.phone }}</a-descriptions-item>
+      <a-descriptions-item label="Company" :span="3">
+        {{ user.company.name }}
+      </a-descriptions-item>
+
+      <a-descriptions-item label="Address">
+        {{ user.address.street }}
+        <br />
+        {{ user.address.suite }}
+        <br />
+        {{ user.address.city }}
+      </a-descriptions-item>
+    </a-descriptions>
+  </a-card>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const user = ref({
-  id: 1,
-  image: "https://via.placeholder.com/150/771796",
-  name: "Leanne Graham",
-  username: "Bret",
-  email: "Sincere@april.biz",
-  address: {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    geo: {
-      lat: "-37.3159",
-      lng: "81.1496",
-    },
-  },
-  phone: "1-770-736-8031 x56442",
-  website: "hildegard.org",
-  company: {
-    name: "Romaguera-Crona",
-    catchPhrase: "Multi-layered client-server neural-net",
-    bs: "harness real-time e-markets",
-  },
-});
+const user = ref(null);
+
+fetch("https://jsonplaceholder.typicode.com/users/5")
+  .then((response) => response.json())
+  .then((data) => (user.value = data));
 </script>
