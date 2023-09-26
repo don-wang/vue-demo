4e60075c151a8f635f4e9d24dc05c22c684e4144
Dongyu Wang <dongyu.wang@monstar-lab.com>
2023-09-26 09:55:37 +0900

    add blogs

--- src/views/hands-on/BlogList.vue
+++ src/views/hands-on/BlogList.vue
@@ -1,5 +1,28 @@
 <template>
-  <p>ここに新しcomponentを挿してください</p>
+  <a-list
+    :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 4 }"
+    :data-source="blogs"
+  >
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a @click.prevent="gotoBlog(item.id)">
+          <a-card :title="item.title"> {{ item.body }}</a-card>
+        </a>
+      </a-list-item>
+    </template>
+  </a-list>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const blogs = ref([]);
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => (blogs.value = data));
+
+const gotoBlog = (id) => {
+  router.push("/hands-on/blogs/" + id);
+};
+</script>
